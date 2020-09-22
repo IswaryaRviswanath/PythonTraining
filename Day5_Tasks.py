02f6b6b86f688f10830f488bc651f0155bf25dd0
IswaryaRviswanath <70594773+IswaryaRviswanath@users.noreply.github.com>
2020-09-22 21:20:34 +0530

    Add files via upload

---
+++ Day5_Tasks.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+Cities = ['Chennai','Kolkatta','Bangalore','Hyderabad']
+
+
+# In[2]:
+
+
+print(Cities)
+
+
+# In[3]:
+
+
+print(sorted(Cities))#temp sorting
+
+
+# In[4]:
+
+
+print(Cities)
+
+
+# In[6]:
+
+
+(Cities).sort() # perm sorting
+
+
+# In[7]:
+
+
+print(Cities)
+
+
+# In[8]:
+
+
+(Cities).reverse() # reverse sorting
+print(Cities)
+
+
+# In[9]:
+
+
+len(Cities) #Return Total count
+
+
+# In[10]:
+
+
+print(Cities[2:4]) #slicing concept
+
+
+# In[11]:
+
+
+print(Cities[1:2:4]) #To display particular set of values
+
+
+# In[ ]:
+
+
+
+
