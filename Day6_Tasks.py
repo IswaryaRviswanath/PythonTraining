2119a907bbf3434ffed01e991eb408143749a76d
IswaryaRviswanath <70594773+IswaryaRviswanath@users.noreply.github.com>
2020-09-23 21:37:50 +0530

    Add files via upload

---
+++ Day6_Tasks.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+For Loop
+
+
+# In[6]:
+
+
+Cities = ['chennai','kolkatta','bangalore','hyderabad']
+
+
+# In[7]:
+
+
+print(Cities)
+
+
+# In[15]:
+
+
+for City in Cities:
+    print(City)
+
+
+# In[16]:
+
+
+for City in Cities:
+    print(City.title())
+
+
+# In[18]:
+
+
+for City in Cities:
+    print(f'Welcome to the city:, {City.title()}, \n')
+
+
+# In[23]:
+
+
+for City in Cities:
+    print(f'Welcome to the city: {City.title()},')
+    print(f'Keep Your city "{City.title()}" clean\n')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
