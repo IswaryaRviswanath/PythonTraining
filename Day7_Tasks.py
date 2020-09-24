bbd007bf45876a6a157a2998f5b52f24de57202e
IswaryaRviswanath <70594773+IswaryaRviswanath@users.noreply.github.com>
2020-09-24 14:58:11 +0530

    Add files via upload

---
+++ Day7_Tasks.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+If Statements:
+
+
+# In[8]:
+
+
+marks = 100
+if marks==100:
+   print (f"Got a true expression value",marks)
+else:
+   print (f"Got a false expression value",marks)
+
+
+# In[9]:
+
+
+marks = 80
+if marks==100:
+   print (f"Got a true expression value",marks)
+else:
+   print (f"Got a false expression value",marks)
+
+
+# In[11]:
+
+
+marks = 100
+if marks==100:
+   print (f"Got A Grade, Marks:",marks)
+elif marks==90:
+   print (f"Got B Grade, Marks:",marks)
+elif marks==80:
+   print (f"Got C Grade, Marks:",marks)
+elif marks==70:
+   print (f"Got D Grade, Marks:",marks)
+elif marks==60:
+   print (f"Got E Grade, Marks:",marks)
+else:
+   print (f"Reappear for exam!!",marks)
+
+
+# In[13]:
+
+
+marks = 50
+if marks==100:
+   print (f"Got A Grade, Marks:",marks)
+elif marks==90:
+   print (f"Got B Grade, Marks:",marks)
+elif marks==80:
+   print (f"Got C Grade, Marks:",marks)
+elif marks==70:
+   print (f"Got D Grade, Marks:",marks)
+elif marks==60:
+   print (f"Got E Grade, Marks:",marks)
+else:
+   print (f"Reappear for exam!!")
+
+
+# In[ ]:
+
+
+
+
