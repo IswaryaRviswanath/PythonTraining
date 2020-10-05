e884d651b2bb8ddb2ce1c5c1ad1ef6a947e11cca
IswaryaRviswanath <70594773+IswaryaRviswanath@users.noreply.github.com>
2020-10-05 20:46:48 +0530

    Add files via upload

---
+++ Day11_Tasks.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+Implementation of Loopong with Dictionaries
+
+
+# In[4]:
+
+
+Fav_City = {
+    'A':'delhi',
+    'B':'bangalore',
+    'C':'hyderabad',
+    'D':'chennai',
+    'E':'kolkatta',
+    
+}
+
+
+# In[5]:
+
+
+print(Fav_City)
+
+
+# In[3]:
+
+
+type(Fav_City)
+
+
+# In[7]:
+
+
+CitySelected = Fav_City['B'].title()
+
+
+# In[8]:
+
+
+print(f"City opted under B is {CitySelected}")
+
+
+# In[ ]:
+
+
+#for loop syntax
+for var in mainvar:
+    print(var)
+
+
+# In[ ]:
+
+
+# for Dict - For Loop
+
+for Key,value in mainvar.items():
+    Print(Key)
+    Print(Key)
+
+
+# In[11]:
+
+
+for option,city in Fav_City.items():
+    print(f"City opted under {option.title()} is {city.title()}")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
